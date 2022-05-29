be10ffd2dfa800ab6ccb69a17baeb19b3e22559c
Levent Sencer Şahin <l.sencer.sahin@gmail.com>
2022-05-29 09:25:56 +0300

    Fix require path for on-time function.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const onDay = require("./src/on-day")
 const onWeek = require("./src/on-week")
 const intervalTime = require("./src/interval-time")
-const onTime = require("./lib/on-time")
+const onTime = require("./src/on-time")
 const onCrontime = require("./src/on-crontime")
 const onDate = require("./src/on-date")
 
