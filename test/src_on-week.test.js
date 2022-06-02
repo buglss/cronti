d3a37608d5c7367e539eaccf661bf0bdc40b898b
Levent Sencer Şahin <l.sencer.sahin@gmail.com>
2022-06-03 01:25:44 +0300

    Add first day of week argument for onTime and onWeek functions.

--- test/src_on-week.test.js
+++ test/src_on-week.test.js
@@ -11,10 +11,18 @@ const onWeek = require("../src/on-week")
 
 describe("onWeek", function() {
     it(`onWeek("2022-05-26T09:30:00.000Z") --> "30 12 22-28 5-5 *" --> Returns the crontime expression that will be triggered at 12:30 on every day of the week it is in, including May 26, 2022, which will be repeated annually.`, function() {
-        assert.strictEqual(onWeek(new Date("2022-05-26 12:30")), "30 12 22-28 5-5 *")
+        assert.strictEqual(onWeek("2022-05-26T09:30:00.000Z"), "30 12 22-28 5-5 *")
     })
 
     it(`onWeek("2022-05-26T09:30:00.000Z", 2) --> "30 12 20-28 5-5 *" --> Returns the crontime expression that will be triggered at 12:30 on every day of the week it is in, including May 26, 2022, which will repeat annually. But the start date is 2 days ago.`, function() {
-        assert.strictEqual(onWeek(new Date("2022-05-26 12:30"), 2), "30 12 20-28 5-5 *")
+        assert.strictEqual(onWeek("2022-05-26T09:30:00.000Z", 2), "30 12 20-28 5-5 *")
+    })
+
+    it(`onWeek("2022-05-26T09:30:00.000Z", "0FD") --> "30 12 21-27 5-5 *" --> Returns the crontime expression that will be triggered at 12:30 on every day of the week it is in, including May 26, 2022, which will be repeated annually.`, function() {
+        assert.strictEqual(onWeek("2022-05-26T09:30:00.000Z", "0FD"), "30 12 21-27 5-5 *")
+    })
+
+    it(`onWeek("2022-05-26T09:30:00.000Z", 2, "0FD") --> "30 12 19-27 5-5 *" --> Returns the crontime expression that will be triggered at 12:30 on every day of the week it is in, including May 26, 2022, which will repeat annually. But the start date is 2 days ago.`, function() {
+        assert.strictEqual(onWeek("2022-05-26T09:30:00.000Z", 2, "0FD"), "30 12 19-27 5-5 *")
     })
 })
